89f87a3fbdd30b60e470ce223ad7656d0a1328d0
Terry <jackhack116@yahoo.com>
2021-06-25 18:15:17 +0800

    finish

--- app.js
+++ app.js
@@ -2,19 +2,26 @@ let GameOver;
 const user = document.querySelector(".user"),
     AI = document.querySelector(".ai"),
     ertext = document.getElementById("text"),
-    com = document.querySelector(".com.flex");
+    com = document.querySelector(".com.flex"),
+    win = document.getElementById("win");
 function initGame() {
     GameOver = false;
-    user.textContent = 0;
+    score[0]=0;
+    score[1]=0;
+    user.textContent = score[0];
+    AI.textContent = score[0];
+    win.textContent = "Let's fight versus noob AI";
+    ertext.textContent = "";
     ai.textContent = 0;
+    com.innerHTML= "";
 }
 let score = [0, 0];
-function initDisplay(up = 0, ap = 0, text = "", aa) {
+function initDisplay(up = 0, ap = 0, aa, text = "") {
     document.getElementById("score").style.visibility = "unset";
     com.style.visibility = "unset";
     AI.textContent = ap;
     user.textContent = up;
-    ertext.textContent = text;
+        ertext.textContent = text;
     com.innerHTML = `<div class="bor"><img src="${aa.url}" alt="${aa.alt}"></div>`;
 
 
@@ -49,24 +56,41 @@ document.getElementById("vs").addEventListener("click", function () {
 
             if (id === AIid) {
 
-                initDisplay(score[0], score[1], "TIED TRY AGAIN", ai[AIid]);
+                initDisplay(score[0], score[1], ai[AIid],"TIED TRY AGAIN");
 
             } else if ((id === 0 && AIid === 1) || (id === 1 && AIid === 2) || (id === 2 && AIid === 0)) {
-                initDisplay(score[0], score[1], ai[AIid]);
+                score[0]++;
+                initDisplay(score[0], score[1], ai[AIid],"Hurray YOU WIN This time");
                 // console.log("ggg");
                 // console.log(AIid);
+            }else{
+                score[1]++;
+                initDisplay(score[0], score[1], ai[AIid],"Greeting Ai xd i win");
             }
 
             console.log(id);
+
         } else {
             alert("Sorry, you need first choose your weapon XD");
+        }
+        if(score[0] ===3)
+        { /* i know it is not dry but this situation XD */
+            GameOver=true;
+            win.textContent = `HURRAY YOU WIN `;
+
+
+        } else if(score[1] === 3){
+            GameOver=true;
+            win.textContent = `Sorry Ai win `;
         }
         // console.log();
 
     }
 });
 
-document.getElementById("res").addEventListener("click", initGame());
+document.getElementById("res").addEventListener("click", ()=>{
+    initGame();
+});
 
 /*change img border */
 document.querySelector(".flex").addEventListener("click", e => {
